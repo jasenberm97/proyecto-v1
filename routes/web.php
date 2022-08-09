150489757db13c0da9e3fa8774a358876eefa3ab
ebrionesdo <ebrionesdo@ulvr.edu.ec>
2022-08-09 18:03:34 -0500

    HTML LOGIN-REGISTER

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login-register');
+});
+
+Route::post('/login', function () {
+    $credentials = request()->only('correo', 'contraseña');
 });
